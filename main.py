ca13c2406db85bdf297957e4c0249193fdda0b7f
Harikrishna A <Harikichus2018@gmail.com>
2023-05-29 23:47:30 +0530

    Added get answer method

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from flask import Flask,request,jsonify
+import mysql.connector as ms
 
 app=Flask(__name__)
+db=ms.connect(host='localhost',user='root',password='H2A0R0I4',database='TRIVIA')
 
-@app.route('/get-user/<user_id>')
-def index(user_id):
-    return f'Hello World {user_id}'
 
-@app.route('/create-user',methods=['POST'])
-def create():
+@app.route('/get-question',methods=['GET'])
+def get_question():
+    cur=db.cursor()
+    cur.execute("SELECT Q_ID,QUESTION FROM TRIVIAS ORDER BY RAND() LIMIT 1")
+    data=cur.fetchone()
+    question_info={
+        'id':data[0],
+        'question':data[1]
+    }
+    cur.close()
+    return jsonify(question_info) ,200
+
+
+@app.route('/get-answer/<ans_id>',methods=['POST'])
+def get_answer(ans_id):
+    cur=db.cursor()
+    cur.execute(f"SELECT ANSWER FROM TRIVIAS WHERE Q_ID={ans_id}")
+    data=cur.fetchone()
+    answer_info={
+        'answer':data[0]
+    }
+    return jsonify(answer_info),200
+
+
+@app.route('/add-question')
+def add_question():
+    return 'question'
+
+
 
 
 if __name__ == '__main__':
